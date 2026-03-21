c329b40ac3c3f4edea82d1b1727262b19a793030
Miguel Castelblanco <64758713+macastelblancog@users.noreply.github.com>
2026-03-21 16:41:06 -0500

    Initial scruture
    Conf files and init for src structure.

---
+++ src/core/__init__.py
@@ -0,0 +1,4 @@
+# src/core/__init__.py
+from src.core.detector import HousePolesDetector
+from src.core.pipeline import Pipeline
+from src.core.schemas import DetectionResult, PipelineResult
---
+++ src/training/__init__.py
@@ -0,0 +1,2 @@
+# src/training/__init__.py
+# intentionally empty
